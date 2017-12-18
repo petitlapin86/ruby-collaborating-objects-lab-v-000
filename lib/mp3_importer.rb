2cefb28b91640a3d1a48fa2320f8ef21ba150dde
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-18 19:30:26 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,7 +1,7 @@
 #The MP3 Importer will parse all the filenames in the db/mp3s folder and send the filenames to the Song class
 
-class MP3Importer #name of class 
-  attr_reader :path #create an attribute reader of path 
+class MP3Importer #name of class
+  attr_reader :path #create an attribute reader of path
 
   def initialize(path) #initialize with path attribute
     @path = path
@@ -9,7 +9,7 @@ class MP3Importer #name of class
 
   def files
     @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
-  end #parses a directory of files 
+  end #parses a directory of files
 
   def import
     files.each{|f| Song.new_by_filename(f)} #this will send files to the song class
