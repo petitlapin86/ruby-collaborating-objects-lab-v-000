a3d0bca530521c4bb61816350adab2b1ed82d9ab
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-18 20:48:00 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,6 +7,15 @@ class Song
     @name = name
   end
 
+  
+    def self.new_by_filename(filename)
+      artist, song = filename.split(" - ") #parse the filename to find song name and artist name 
+      new_song = self.new(song) #create a new song instance 
+      new_song.artist_name = artist #associate the new song with an artist 
+      new_song #return the new song 
+    end
+
+
   def artist_name(name)
     self.artist = Artist.find_or_create_by_name(name) #send artist name to artist class
        artist.add_song(self) #assign song to artist
