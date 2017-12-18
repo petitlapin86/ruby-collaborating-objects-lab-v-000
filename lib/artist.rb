7c087e07d13290121e75beebd0c0c481d7ce90c2
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-18 20:00:58 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,7 @@
 
 
-#The Artist class will be responsible for either creating the artist 
-#(if the artist doesn't exist in our program yet) 
+#The Artist class will be responsible for either creating the artist
+#(if the artist doesn't exist in our program yet)
 #or finding the instance of that artist (if the artist does exist).
 
 class Artist
---
+++ lib/song.rb
@@ -0,0 +1,3 @@
+#The Song class will be responsible for creating songs given each filename and sending the artist's name (a string) to the Artist class
+class Song
+end 
