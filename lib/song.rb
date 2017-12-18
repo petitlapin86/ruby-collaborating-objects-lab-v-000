41c5a661e3448ee5107b7d2d20886c9c419c10a6
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-18 20:16:37 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,9 @@
 #The Song class will be responsible for creating songs given each filename and sending the artist's name (a string) to the Artist class
 class Song
 
-  attr_accessor :name 
+  attr_accessor :name
   
   def initialize(name)
     @name = name
   end
-end 
+end
