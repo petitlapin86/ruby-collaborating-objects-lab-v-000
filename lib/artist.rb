5b6aca31d95b21f6aa952d8df2d4706ad4b5d025
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-18 20:00:46 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,9 @@
+
+
+#The Artist class will be responsible for either creating the artist 
+#(if the artist doesn't exist in our program yet) 
+#or finding the instance of that artist (if the artist does exist).
+
+class Artist
+
+end 
